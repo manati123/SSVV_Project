38d442f01d64ce8c4e2aef847a38375905351bff
Bodo171 <pbppbp17@gmail.com>
2022-03-14 08:55:50 +0200

    Update AppTest.java

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -2,7 +2,18 @@ package ssvv.example;
 
 import static org.junit.Assert.assertTrue;
 
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
 import org.junit.Test;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
 
 /**
  * Unit test for simple App.
@@ -16,5 +27,20 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void AddStudentInvalidGroupNumber(){
+        /*Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);*/
+
+        assert(true);
     }
 }
