d75fc17b5570d6ffe95f2223f6d1c2ccc579e5e8
Bodo171 <pbppbp17@gmail.com>
2022-03-26 13:38:46 +0200

    File infrastructure + invalid student tests

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -1,26 +1,36 @@
 package ssvv.example;
 
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
-
 import domain.Nota;
 import domain.Student;
 import domain.Tema;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.rules.TemporaryFolder;
 import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
 import service.Service;
-import validation.NotaValidator;
-import validation.StudentValidator;
-import validation.TemaValidator;
-import validation.Validator;
+import validation.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+    Service service;
     /**
      * Rigorous Test :-)
      */
@@ -30,19 +40,81 @@ public class AppTest
         assertTrue( true );
     }
 
-    @Test
-    public void AddStudentInvalidGroupNumber(){
-        /*Validator<Student> studentValidator = new StudentValidator();
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @TempDir
+    public Path tempDir;
+
+    @Before
+    public void setUpTest(){
+        Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
         Validator<Nota> notaValidator = new NotaValidator();
 
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+        Path path1, path2, path3;
+        /*File file1 = folder.newFile( "studenti_test.xml" );
+         File file2 = folder.newFile( "teme_test.xml" );
+           File file3 = folder.newFile( "note_test.xml" );*/
+        /*path1 = tempDir.resolve( "testfile1.xml" );
+        path2 = tempDir.resolve( "testfile2.xml" );
+        path3 = tempDir.resolve( "testfile3.xml" );*/
+        File a = new File("testfile1.xml");
+        File b = new File("testfile2.xml");
+        File c = new File("testfile3.xml");
+        String empty = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?> <Entitati></Entitati>";
+        try {
+            a.createNewFile();
+            b.createNewFile();
+            c.createNewFile();
+            FileWriter fwa = new FileWriter(a);
+            fwa.write(empty);
+            FileWriter fwb = new FileWriter(b);
+            fwb.write(empty);
+            FileWriter fwc = new FileWriter(c);
+            fwc.write(empty);
+            fwa.close();fwb.close();fwc.close();
+        } catch (IOException ioe){
+            System.out.println("exception");
+            return;
+        }
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "testfile1.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "testfile2.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "testfile3.xml");
 
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);*/
+        this.service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        System.out.println("ok");
+    }
 
-        assert(true);
+    @After
+    public void tearDown(){
+        File a = new File("testfile1.xml");
+        File b = new File("testfile2.xml");
+        File c = new File("testfile3.xml");
+        a.delete();b.delete();c.delete();
+    }
+
+    @Test
+    public void addStudentInvalidGroupNumber(){
+        boolean raised = false;
+        assertEquals(1, service.saveStudent("1", "aa", 939));
+        assertEquals(1, service.saveStudent("1", "aa", 109));
+        Iterable<Student> students = service.findAllStudents();
+        long studentCount = StreamSupport.stream(students.spliterator(), false).count();
+        assertEquals(0, studentCount);
+    }
+
+    @Test
+    public void addStudentInvalidName(){
+        assertEquals(1, service.saveStudent("1", null, 939));
+        assertEquals(1, service.saveStudent("1", "", 118));
+    }
+
+    @Test
+    public void addStudentInvalidId(){
+        boolean raised = false;
+        assertEquals(1, service.saveStudent("0", "john", 939));
+        assertEquals(1, service.saveStudent(null, "john", 118));
     }
 
     @Test
